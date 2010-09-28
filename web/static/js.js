e17290f6422d06270eb25af1e2655d00a6eb1b29
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-28 16:57:30 +0100

    Map start.

---
+++ web/static/js.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('#id_postcode').change(function(){
+        var val = $(this).val();
+        $.getJSON('http://mapit.mysociety.org/postcode/' + encodeURIComponent(val), function(data){
+            alert(data['wgs84_lat']);
+        });
+    });
+
+    $("input[@name='radius']").click(function(){
+    });
+
+});
