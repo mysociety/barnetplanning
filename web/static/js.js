f57102dd9e565b5c253cd6912e2fea575fca8558
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-29 00:06:41 +0100

    Map levels, start points.

--- web/static/js.js
+++ web/static/js.js
@@ -2,15 +2,24 @@ $(function(){
 
     if ($('#map').length) {
         var map = new OpenLayers.Map('map');
-        var layer = new OpenLayers.Layer.OSM('OS StreetView', 'http://os.openstreetmap.org/sv/${z}/${x}/${y}.png');
+        var layer = new OpenLayers.Layer.OSM('OS StreetView', 'http://os.openstreetmap.org/sv/${z}/${x}/${y}.png', {
+            minZoomLevel: 6,
+            maxZoomLevel: 18
+        });
         map.addLayer(layer);
-        map.zoomToMaxExtent();
+        map.setCenter(new OpenLayers.LonLat(-0.2005, 51.652963).transform(
+            new OpenLayers.Projection("EPSG:4326"),
+            new OpenLayers.Projection("EPSG:900913")
+        ), 12);
     }
 
     $('#id_postcode').change(function(){
         var val = $(this).val();
         $.getJSON('http://mapit.mysociety.org/postcode/' + encodeURIComponent(val) + '?callback=?',  function(data){
-            alert(data['wgs84_lat']);
+            map.setCenter(new OpenLayers.LonLat(data['wgs84_lon'], data['wgs84_lat']).transform(
+                new OpenLayers.Projection("EPSG:4326"),
+                new OpenLayers.Projection("EPSG:900913")
+            ), 15);
         });
     });
 
