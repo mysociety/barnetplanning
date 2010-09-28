71b68cc90d77b91c8095c128b59fce88f74eae61
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-28 23:57:02 +0100

    Show map.

--- web/static/js.js
+++ web/static/js.js
@@ -1,5 +1,12 @@
 $(function(){
 
+    if ($('#map').length) {
+        var map = new OpenLayers.Map('map');
+        var layer = new OpenLayers.Layer.OSM('OS StreetView', 'http://os.openstreetmap.org/sv/${z}/${x}/${y}.png');
+        map.addLayer(layer);
+        map.zoomToMaxExtent();
+    }
+
     $('#id_postcode').change(function(){
         var val = $(this).val();
         $.getJSON('http://mapit.mysociety.org/postcode/' + encodeURIComponent(val) + '?callback=?',  function(data){
