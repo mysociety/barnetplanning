5d7274b92363e9059e4bed34d39eb298090b8d13
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-28 17:06:02 +0100

    Try empty data string.

--- web/static/js.js
+++ web/static/js.js
@@ -2,7 +2,7 @@ $(function(){
 
     $('#id_postcode').change(function(){
         var val = $(this).val();
-        $.getJSON('http://mapit.mysociety.org/postcode/' + encodeURIComponent(val), function(data){
+        $.getJSON('http://mapit.mysociety.org/postcode/' + encodeURIComponent(val), '', function(data){
             alert(data['wgs84_lat']);
         });
     });
