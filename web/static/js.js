e4906917765d46b7380ac49999d9e55420b87a2f
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-28 17:25:06 +0100

    Try callback.

--- web/static/js.js
+++ web/static/js.js
@@ -2,7 +2,7 @@ $(function(){
 
     $('#id_postcode').change(function(){
         var val = $(this).val();
-        $.getJSON('http://mapit.mysociety.org/postcode/' + encodeURIComponent(val), '', function(data){
+        $.getJSON('http://mapit.mysociety.org/postcode/' + encodeURIComponent(val) + '?callback=?',  function(data){
             alert(data['wgs84_lat']);
         });
     });
