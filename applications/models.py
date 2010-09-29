bd0932667db1215de42cfa223a869f4871a797e8
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-29 20:52:24 +0100

    Allow null location, go back five days.

--- applications/models.py
+++ applications/models.py
@@ -4,7 +4,7 @@ class Application(models.Model):
     council_reference = models.CharField(max_length=50)
     address = models.TextField()
     postcode = models.CharField(max_length=8)
-    location = models.PointField()
+    location = models.PointField(null=True)
     description = models.TextField()
     info_url = models.CharField(max_length=1024)
     created = models.DateTimeField(auto_now_add=True)
