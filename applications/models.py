b4ced84b2d9d3036df65cf51804a14a9cebc48a0
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-29 18:03:38 +0100

    Import applications into db script.

---
+++ applications/models.py
@@ -0,0 +1,15 @@
+from django.contrib.gis.db import models
+
+class Application(models.Model):
+    council_reference = models.CharField(max_length=50)
+    address = models.TextField()
+    postcode = models.CharField(max_length=8)
+    location = models.PointField()
+    description = models.TextField()
+    info_url = models.CharField(max_length=1024)
+    created = models.DateTimeField(auto_now_add=True)
+    received = models.DateField()
+
+    def __unicode__(self):
+        return 'Planning application for %s (ref %s), made %s' % (self.address, self.council_reference, self.received)
+
