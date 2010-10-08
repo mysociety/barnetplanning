9c9eb7e4219f5d98107f49c032a5b73467549f19
Francis Irving <francis@mysociety.org>
2010-10-08 12:15:15 +0100

    Use emailconfirmation from commonlib.

--- emailconfirmation/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('emailconfirmation.views',
-    url(r'^C/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)$', 'check_token', name='emailconfirmation-check-token'),
-)
-
